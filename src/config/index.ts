9fbddd58cc13c98fa330d64d35aeb7898a2610aa
Piotr Kulpinski <piotr@kulpinski.pl>
2024-02-29 14:24:44 +0100

    implement better view transitions and clean up layouts and components

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const config = {
-  title: "Open Source Alternatives",
+  title: "OpenAlternative",
   tagline: "The easiest way to collect anything online",
   description:
     "Putting together a directory website was time consuming. InputNest lets you focus on the content and makes the entire process effortless.",
