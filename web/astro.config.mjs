31fddee7ca262e9c2e9e26ce23f3a5514d8bd0f3
Piotr Kulpinski <piotr@kulpinski.pl>
2024-03-03 00:22:49 +0100

    add automatic sitemap generation

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,7 +1,9 @@
+import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: process.env.SITE_URL || "https://openalternative.local",
+  integrations: [tailwind(), sitemap()],
 })
