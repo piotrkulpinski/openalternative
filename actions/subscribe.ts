facd4ace04dfeabb031af2f542b145b393ff725a
Piotr Kulpinski <piotr@kulpinski.pl>
2024-03-18 09:37:44 +0100

    update subscribe form and fix issues with urls with search params

--- actions/subscribe.ts
+++ actions/subscribe.ts
@@ -6,11 +6,13 @@ import { env } from "~/env"
 
 const subscriberSchema = z.object({
   email: z.string().email().min(1),
+  groups: z.array(z.string()).optional(),
 })
 
 export const subscribe = async (formData: FormData) => {
   const subscriber = subscriberSchema.parse({
     email: formData.get("email"),
+    groups: ["115156291506144329"],
   })
 
   const response = await ky.post("https://connect.mailerlite.com/api/subscribers", {
