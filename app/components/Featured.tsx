4cd6ffc07a231e14060a0cda35013ea8ae7e1569
Piotr Kulpinski <piotr@kulpinski.pl>
2024-03-20 17:34:45 +0100

    add indie hackers post to the featured section

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -6,7 +6,7 @@ export const Featured = ({ className, ...props }: HTMLAttributes<HTMLElement>) =
     <div className={cx("space-y-2", className)} {...props}>
       <h4 className="text-[10px] uppercase text-gray-500">Featured on</h4>
 
-      <div className="flex flex-wrap items-center gap-x-4 md:gap-x-6">
+      <div className="flex flex-wrap items-center gap-x-4 gap-y-2 md:gap-x-6">
         <a
           href="https://www.producthunt.com/posts/openalternative"
           target="_blank"
@@ -47,6 +47,22 @@ export const Featured = ({ className, ...props }: HTMLAttributes<HTMLElement>) =
         >
           <img src="/twitter.svg" width="24" height="24" alt="Twitter" className="rounded-full" />
           Twitter
+        </a>
+
+        <a
+          href="https://www.indiehackers.com/post/how-i-grew-a-side-project-to-100k-unique-visitors-in-7-days-with-0-audience-15d48ea192"
+          target="_blank"
+          rel="nofollow noreferrer"
+          className="flex flex-wrap items-center gap-x-2 text-sm font-medium opacity-60 grayscale hover:opacity-100 hover:grayscale-0"
+        >
+          <img
+            src="/indiehackers.svg"
+            width="24"
+            height="24"
+            alt="Indie Hackers"
+            className="rounded-full"
+          />
+          Indie Hackers
         </a>
       </div>
     </div>
