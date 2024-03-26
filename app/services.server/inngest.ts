6eb8b43fbe98bed6d0e58baa7f315733acaa965e
Piotr Kulpinski <piotr@kulpinski.pl>
2024-03-26 11:36:51 +0100

    add fix for inngest functions on vercel

--- app/services.server/inngest.ts
+++ app/services.server/inngest.ts
@@ -1,8 +1,10 @@
+import { fetch as webFetch } from "@remix-run/web-fetch"
 import { EventSchemas, Inngest } from "inngest"
 
 import type { Events } from "~/functions.server"
 
 export const inngest = new Inngest({
   id: "openalternative",
+  fetch: webFetch as typeof fetch,
   schemas: new EventSchemas().fromRecord<Events>(),
 })
