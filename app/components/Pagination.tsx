67a51692e379d1f94dea4b5e659f8b8cb0d8abb6
Piotr Kulpinski <piotr@kulpinski.pl>
2024-03-27 23:39:30 +0100

    update the pagination links

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,9 +1,9 @@
 import { getCurrentPage, getPageLink } from "@curiousleaf/utils"
-import { NavLink, useLocation, useSearchParams } from "@remix-run/react"
+import { useLocation, useSearchParams } from "@remix-run/react"
 import { MoveLeftIcon, MoveRightIcon } from "lucide-react"
 import { HTMLAttributes, useMemo } from "react"
 import { cx } from "~/utils/cva"
-import { navigationLinkVariants } from "./NavigationLink"
+import { PaginationLink } from "./PaginationLink"
 
 export type PaginationProps = HTMLAttributes<HTMLElement> & {
   totalCount: number
@@ -26,33 +26,23 @@ export const Pagination = ({ className, totalCount, pageSize = 0, ...props }: Pa
       className={cx("-mt-px flex w-full items-start justify-between md:w-auto", className)}
       {...props}
     >
-      <NavLink
+      <PaginationLink
         to={getPageLink(params, pathname, currentPage - 1)}
-        className={cx(
-          navigationLinkVariants({
-            className: currentPage <= 1 && "pointer-events-none select-none opacity-40",
-          })
-        )}
-        unstable_viewTransition
+        disabled={currentPage <= 1}
+        prefix={<MoveLeftIcon />}
         rel="prev"
       >
-        <MoveLeftIcon className="size-5" />
-        <span className="max-md:sr-only">Previous</span>
-      </NavLink>
+        previous
+      </PaginationLink>
 
-      <NavLink
+      <PaginationLink
         to={getPageLink(params, pathname, currentPage + 1)}
-        className={cx(
-          navigationLinkVariants({
-            className: currentPage >= pageCount && "pointer-events-none select-none opacity-40",
-          })
-        )}
-        unstable_viewTransition
-        rel="next"
+        disabled={currentPage >= pageCount}
+        suffix={<MoveRightIcon />}
+        rel="prev"
       >
-        <span className="max-md:sr-only">Next</span>
-        <MoveRightIcon className="size-5" />
-      </NavLink>
+        next
+      </PaginationLink>
     </nav>
   )
 }
