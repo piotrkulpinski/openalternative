c9903c19ac52bd1a6b28f98e24a8344eb27c616f
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-02 13:57:27 +0200

    update meta title and description of the homepage

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,9 +1,9 @@
 export const SITE_URL = import.meta.env.VITE_SITE_URL
 export const SITE_EMAIL = import.meta.env.VITE_SITE_EMAIL
 export const SITE_NAME = "OpenAlternative"
-export const SITE_TAGLINE = "Discover Open Source Alternatives to Popular Software"
+export const SITE_TAGLINE = "Open Source Alternatives to Popular Software"
 export const SITE_DESCRIPTION =
-  "OpenAlternative is a platform that helps you find open source alternatives to popular software. We’ve curated some great open source alternatives to tools that your business requires in day-to-day operations."
+  "OpenAlternative is a community-driven list of open source alternatives to proprietary software and applications. Discover the best tools for your needs."
 
 export const JSON_HEADERS =
   process.env.NODE_ENV === "development"
