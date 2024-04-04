27332ae17b2a60f9f57ea0689fb27562c14028e7
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-04 14:48:38 +0200

    add social links to the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -24,12 +24,52 @@ export const Footer = ({ children, className, ...props }: HTMLAttributes<HTMLEle
         >
           Contact
         </NavigationLink>
+
+        <NavigationLink
+          to="https://twitter.com/ossalternative"
+          target="_blank"
+          title="Twitter"
+          rel="nofollow noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="size-[1.44em]"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+            <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+          </svg>
+        </NavigationLink>
+
         <NavigationLink
           to="https://github.com/piotrkulpinski/openalternative"
           target="_blank"
+          title="Source Code"
           rel="nofollow noreferrer"
         >
-          Source Code
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="size-[1.44em]"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+          </svg>
         </NavigationLink>
       </Series>
 
