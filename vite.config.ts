64be8830f205f0c320bb51efddcc07d454809241
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-05 12:40:24 +0200

    remove mdx packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,10 @@ import { vitePlugin as remix } from "@remix-run/dev"
 import { installGlobals } from "@remix-run/node"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
-import mdx from "@mdx-js/rollup"
-import remarkFrontmatter from "remark-frontmatter"
-import remarkMdxFrontmatter from "remark-mdx-frontmatter"
 import { vercelPreset } from "@vercel/remix/vite"
 
 installGlobals()
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    mdx({ remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter] }),
-    remix({ presets: [vercelPreset()] }),
-  ],
+  plugins: [tsconfigPaths(), remix({ presets: [vercelPreset()] })],
 })
