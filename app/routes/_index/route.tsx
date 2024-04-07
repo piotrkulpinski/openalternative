c09ef11b97c19f995c5b99d15f512e63c92e6c12
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-08 00:15:27 +0200

    switch to client-side fetching tools on the homepage

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,22 +1,19 @@
 import { getPageParams } from "@curiousleaf/utils"
 import { Prisma } from "@prisma/client"
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node"
-import { Await, NavLink, defer, useLoaderData } from "@remix-run/react"
+import { NavLink, defer, useLoaderData } from "@remix-run/react"
 import { BlocksIcon, BracesIcon, TagIcon } from "lucide-react"
-import { Suspense } from "react"
 import { Button } from "~/components/Button"
 import { Filters } from "~/components/Filters"
-import { Grid } from "~/components/Grid"
 import { Intro } from "~/components/Intro"
 import { Newsletter } from "~/components/Newsletter"
-import { Pagination } from "~/components/Pagination"
 import { Series } from "~/components/Series"
-import { ToolRecord, ToolRecordSkeleton } from "~/components/records/ToolRecord"
 import { toolManyPayload } from "~/services.server/api"
 import { prisma } from "~/services.server/prisma"
 import { JSON_HEADERS, SITE_DESCRIPTION, SITE_TAGLINE, TOOLS_PER_PAGE } from "~/utils/constants"
 import { getSearchQuery, isMobileAgent } from "~/utils/helpers"
 import { getMetaTags } from "~/utils/meta"
+import { Listing } from "./Listing"
 
 export const meta: MetaFunction<typeof loader> = ({ matches }) => {
   return getMetaTags({
@@ -96,7 +93,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 }
 
 export default function Index() {
-  const { tools, toolCount, postsPerPage } = useLoaderData<typeof loader>()
+  const { postsPerPage } = useLoaderData<typeof loader>()
 
   return (
     <>
@@ -110,58 +107,32 @@ export default function Index() {
         <Newsletter placeholder="Get weekly newsletter" buttonVariant="fancy" />
       </section>
 
-      <Grid>
-        <div className="col-span-full flex flex-wrap items-center justify-between gap-4">
-          <Series size="sm" className="flex-nowrap max-sm:gap-1.5">
-            <Button size="md" variant="secondary" prefix={<BlocksIcon />} asChild>
-              <NavLink to="/categories" prefetch="intent" unstable_viewTransition>
-                <span className="max-sm:hidden">Browse by Category</span>
-                <span className="sm:hidden">Categories</span>
-              </NavLink>
-            </Button>
-            <Button size="md" variant="secondary" prefix={<BracesIcon />} asChild>
-              <NavLink to="/languages" prefetch="intent" unstable_viewTransition>
-                <span className="max-sm:hidden">Browse by Language</span>
-                <span className="sm:hidden">Languages</span>
-              </NavLink>
-            </Button>
-            <Button size="md" variant="secondary" prefix={<TagIcon />} asChild>
-              <NavLink to="/topics" prefetch="intent" unstable_viewTransition>
-                <span className="max-sm:hidden">Browse by Topic</span>
-                <span className="sm:hidden">Topics</span>
-              </NavLink>
-            </Button>
-          </Series>
+      <div className="col-span-full flex flex-wrap items-center justify-between gap-4">
+        <Series size="sm" className="flex-nowrap max-sm:gap-1.5">
+          <Button size="md" variant="secondary" prefix={<BlocksIcon />} asChild>
+            <NavLink to="/alternatives" prefetch="intent" unstable_viewTransition>
+              <span className="max-sm:hidden">Browse by Alternative</span>
+              <span className="sm:hidden">Alternatives</span>
+            </NavLink>
+          </Button>
+          <Button size="md" variant="secondary" prefix={<BracesIcon />} asChild>
+            <NavLink to="/languages" prefetch="intent" unstable_viewTransition>
+              <span className="max-sm:hidden">Browse by Language</span>
+              <span className="sm:hidden">Languages</span>
+            </NavLink>
+          </Button>
+          <Button size="md" variant="secondary" prefix={<TagIcon />} asChild>
+            <NavLink to="/topics" prefetch="intent" unstable_viewTransition>
+              <span className="max-sm:hidden">Browse by Topic</span>
+              <span className="sm:hidden">Topics</span>
+            </NavLink>
+          </Button>
+        </Series>
 
-          <Filters />
-        </div>
+        <Filters />
+      </div>
 
-        <Suspense
-          fallback={Array.from({ length: 9 }).map((_, i) => (
-            <ToolRecordSkeleton key={i} />
-          ))}
-        >
-          <Await resolve={tools}>
-            {(tools) => (
-              <>
-                {tools.map((tool) => (
-                  <ToolRecord key={tool.id} tool={tool} />
-                ))}
-
-                {!tools.length && <p>No tools found.</p>}
-              </>
-            )}
-          </Await>
-        </Suspense>
-      </Grid>
-
-      <Suspense>
-        <Await resolve={toolCount}>
-          {(toolCount) => (
-            <Pagination totalCount={toolCount} pageSize={postsPerPage} className="col-span-full" />
-          )}
-        </Await>
-      </Suspense>
+      <Listing />
     </>
   )
 }
