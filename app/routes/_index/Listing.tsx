324257fd195f653df22a7553233c791bb8eb5e90
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-09 16:53:19 +0200

    add empty results message

--- app/routes/_index/Listing.tsx
+++ app/routes/_index/Listing.tsx
@@ -2,6 +2,7 @@ import { useHits, type UseHitsProps } from "react-instantsearch"
 import { Grid } from "~/components/Grid"
 import { ToolOne } from "~/services.server/api"
 import { ToolRecord } from "~/components/records/ToolRecord"
+import { H5 } from "~/components/Heading"
 
 export const Listing = ({ ...props }: UseHitsProps<ToolOne>) => {
   const { hits, sendEvent } = useHits(props)
@@ -16,6 +17,8 @@ export const Listing = ({ ...props }: UseHitsProps<ToolOne>) => {
           onAuxClick={() => sendEvent("click", hit, "Hit Clicked")}
         />
       ))}
+
+      {!hits.length && <H5>No results found</H5>}
     </Grid>
   )
 }
