001b4f44edd442457bbb1b95cfda6e354f9980a5
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-10 17:14:58 +0200

    add trusted by section to the sponsorship page

--- app/components/SponsoredCard.tsx
+++ app/components/SponsoredCard.tsx
@@ -1,30 +1,36 @@
 import { Link } from "@remix-run/react"
 import { Button } from "./Button"
 import { CardProps, Card } from "./Card"
-import { H3 } from "./Heading"
+import { H4 } from "./Heading"
 import { cx } from "~/utils/cva"
-import { Favicon } from "./Favicon"
+import { Logo } from "./Logo"
 
 export const SponsoredCard = ({ className, ...props }: CardProps) => {
   return (
     <Card
-      className={cx("!border-neutral-300 order-2 hover:bg-neutral-100", className)}
+      className={cx(
+        "!border-neutral-300 order-2 hover:bg-neutral-100 dark:!border-neutral-600 dark:hover:!bg-neutral-800",
+        className
+      )}
       asChild
       {...props}
     >
       <Link to="/sponsor">
         <Card.Header>
-          <Favicon src="/favicon.svg" />
-          <H3 className="truncate">Become a sponsor</H3>
+          <H4 as="h2" className="truncate">
+            Sponsor OpenAlternative
+          </H4>
         </Card.Header>
 
-        <Card.Description className="my-auto line-clamp-4 ">
+        <Card.Description className="mb-auto line-clamp-4 ">
           Reach out to our audience of professional developers and designers.
         </Card.Description>
 
         <Button className="w-full pointer-events-none" asChild>
-          <span>Learn more</span>
+          <span>Become a sponsor</span>
         </Button>
+
+        <Logo className="absolute -bottom-1/4 -right-1/4 size-64 opacity-[3.5%] rotate-45 pointer-events-none" />
       </Link>
     </Card>
   )
--- app/components/records/AlternativeRecord.tsx
+++ app/components/records/AlternativeRecord.tsx
@@ -5,7 +5,7 @@ import { AlternativeMany } from "~/services.server/api"
 import { SerializeFrom } from "@remix-run/node"
 import { NavLink } from "@remix-run/react"
 import { Favicon } from "../Favicon"
-import { H3 } from "../Heading"
+import { H4 } from "../Heading"
 
 type AlternativeRecordProps = HTMLAttributes<HTMLElement> & {
   alternative: SerializeFrom<AlternativeMany>
@@ -19,7 +19,9 @@ export const AlternativeRecord = ({ alternative, showCount, ...props }: Alternat
         <Card.Header>
           <Favicon src={alternative.faviconUrl} title={alternative.name} />
 
-          <H3 className="truncate">{alternative.name}</H3>
+          <H4 as="h3" className="truncate">
+            {alternative.name}
+          </H4>
         </Card.Header>
 
         {alternative.description && <Card.Description>{alternative.description}</Card.Description>}
