c57b081f823b3721cf64a8a5bf7037b699ed6a15
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-10 13:59:05 +0200

    clean up card styles

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,7 +2,7 @@ import { HTMLAttributes, forwardRef, isValidElement } from "react"
 import { cx } from "~/utils/cva"
 import { Slot } from "@radix-ui/react-slot"
 
-type CardProps = HTMLAttributes<HTMLElement> & {
+export type CardProps = HTMLAttributes<HTMLElement> & {
   /**
    * If set to `true`, the button will be rendered as a child within the component.
    * This child component must be a valid React component.
@@ -20,7 +20,8 @@ export const CardBase = forwardRef<HTMLDivElement, CardProps>(({ ...props }, ref
     <Component
       ref={ref}
       className={cx(
-        "fade-in relative flex flex-col items-start gap-4 overflow-clip rounded-md border bg-neutral-50 p-5 hover:border-neutral-300 dark:border-neutral-700/50 dark:bg-neutral-800/40 dark:hover:border-neutral-700",
+        "fade-in relative flex flex-col items-start gap-4 overflow-clip rounded-md border bg-neutral-50 p-5 dark:border-neutral-700/50 dark:bg-neutral-800/40",
+        "hover:[&[href]]:border-neutral-300 group-hover-[&[href]]:border-neutral-300 dark:hover:[&[href]]:border-neutral-700 dark:group-hover-[&[href]]:border-neutral-700",
         className
       )}
       {...rest}
@@ -32,8 +33,23 @@ export const CardBase = forwardRef<HTMLDivElement, CardProps>(({ ...props }, ref
 
 CardBase.displayName = "Card"
 
-export const CardHeader = ({ className, ...rest }: HTMLAttributes<HTMLElement>) => {
-  return <div className={cx("flex w-full items-center gap-x-3 gap-y-2", className)} {...rest} />
+export const CardHeader = ({ className, ...props }: HTMLAttributes<HTMLElement>) => {
+  return <div className={cx("flex w-full items-center gap-x-3 gap-y-2", className)} {...props} />
 }
 
-export const Card = Object.assign(CardBase, { Header: CardHeader })
+export const CardDescription = ({ className, ...props }: HTMLAttributes<HTMLElement>) => {
+  return (
+    <p
+      className={cx(
+        "-tracking-0.5 line-clamp-2 text-sm/normal text-neutral-600 dark:text-neutral-400",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export const Card = Object.assign(CardBase, {
+  Header: CardHeader,
+  Description: CardDescription,
+})
--- app/components/SponsoredCard.tsx
+++ app/components/SponsoredCard.tsx
@@ -3,24 +3,22 @@ import { Button } from "./Button"
 import { CardProps, Card } from "./Card"
 import { H3 } from "./Heading"
 import { cx } from "~/utils/cva"
+import { Favicon } from "./Favicon"
 
 export const SponsoredCard = ({ className, ...props }: CardProps) => {
   return (
     <Card
-      className={cx(
-        "!border-neutral-300 order-2 text-center items-center hover:bg-neutral-100",
-        className
-      )}
+      className={cx("!border-neutral-300 order-2 hover:bg-neutral-100", className)}
       asChild
       {...props}
     >
       <Link to="/sponsor">
-        {/* <Card.Header> */}
-        {/* <Favicon src="/favicon.svg" /> */}
-        <H3 className="truncate text-center">Become a sponsor</H3>
-        {/* </Card.Header> */}
+        <Card.Header>
+          <Favicon src="/favicon.svg" />
+          <H3 className="truncate">Become a sponsor</H3>
+        </Card.Header>
 
-        <Card.Description className="text-center my-auto line-clamp-4 max-w-[20ch]">
+        <Card.Description className="my-auto line-clamp-4 ">
           Reach out to our audience of professional developers and designers.
         </Card.Description>
 
