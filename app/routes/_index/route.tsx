a56bcb11a559c3b68a9d5e2773253c0f06364cf4
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-11 16:19:47 +0200

    add product hunt launch card to support open source software

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -10,6 +10,7 @@ import { Search } from "./Search"
 import { prisma } from "~/services.server/prisma"
 import { Badge } from "~/components/Badge"
 import { Ping } from "~/components/Ping"
+import { ProductHuntCard } from "~/components/ProductHuntCard"
 
 export const meta: MetaFunction = ({ matches }) => {
   return getMetaTags({
@@ -36,23 +37,27 @@ export default function Index() {
 
   return (
     <>
-      <section className="flex flex-col gap-y-6">
-        <Intro
-          title="Discover Open Source Alternatives to Popular Software"
-          description="We’ve curated some great open source alternatives to tools that your business requires in day-to-day operations."
-          className="max-w-[40rem] text-pretty"
-        >
-          {newToolCount && (
-            <Link to="/latest" className="contents">
-              <Badge className="order-first inline-flex items-center gap-1.5 px-2 py-1 rounded-md">
-                <Ping /> {newToolCount} tools added this week
-              </Badge>
-            </Link>
-          )}
-        </Intro>
+      <div className="flex gap-6">
+        <section className="flex flex-col gap-y-6 md:flex-1">
+          <Intro
+            title="Discover Open Source Alternatives to Popular Software"
+            description="We’ve curated some great open source alternatives to tools that your business requires in day-to-day operations."
+            className="max-w-[40rem] text-pretty"
+          >
+            {newToolCount && (
+              <Link to="/latest" className="contents">
+                <Badge className="order-first inline-flex items-center gap-1.5 px-2 py-1 rounded-md">
+                  <Ping /> {newToolCount} tools added this week
+                </Badge>
+              </Link>
+            )}
+          </Intro>
 
-        <Newsletter placeholder="Get weekly newsletter" buttonVariant="fancy" />
-      </section>
+          <Newsletter placeholder="Get weekly newsletter" buttonVariant="fancy" />
+        </section>
+
+        <ProductHuntCard className="max-md:hidden md:w-60" />
+      </div>
 
       <InstantSearchSSRProvider key={key} {...serverState}>
         <Search url={url} />
