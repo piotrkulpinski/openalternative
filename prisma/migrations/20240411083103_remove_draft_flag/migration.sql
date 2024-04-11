9d191f99a18a2a0b63abf75c08454b8c8a2a94e5
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-11 10:46:43 +0200

    remove the isDraft flag from the db

---
+++ prisma/migrations/20240411083103_remove_draft_flag/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isDraft` on the `Tool` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Tool" DROP COLUMN "isDraft";
