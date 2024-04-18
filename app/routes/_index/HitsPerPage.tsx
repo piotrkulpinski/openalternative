80cba5c20ce8274f423f2a4d8ca35c74036f4861
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-18 11:15:44 +0200

    update search functionality to display algolia logo in the search bar

--- app/routes/_index/HitsPerPage.tsx
+++ app/routes/_index/HitsPerPage.tsx
@@ -1,16 +1,18 @@
+import { HTMLAttributes } from "react"
 import { useHitsPerPage, type UseHitsPerPageProps } from "react-instantsearch"
 import { Select } from "~/components/forms/Select"
 
-export const HitsPerPage = ({ ...props }: UseHitsPerPageProps) => {
+type HitsPerPageProps = HTMLAttributes<HTMLElement> & UseHitsPerPageProps
+
+export const HitsPerPage = ({ className, ...props }: HitsPerPageProps) => {
   const { items, refine } = useHitsPerPage(props)
   const { value: currentValue } = items.find(({ isRefined }) => isRefined) || {}
 
   return (
     <Select
-      onChange={(e) => {
-        refine(Number(e.target.value))
-      }}
+      onChange={(e) => refine(Number(e.target.value))}
       value={String(currentValue)}
+      className={className}
     >
       <option value="" disabled>
         Hits per page
