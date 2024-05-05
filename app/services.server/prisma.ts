8d3119f28db0455f479009339d109e407f56695c
Piotr Kulpinski <piotr@kulpinski.pl>
2024-05-05 23:15:33 +0200

    remove chalk from prisma client log

--- app/services.server/prisma.ts
+++ app/services.server/prisma.ts
@@ -1,6 +1,5 @@
 import { remember } from "@epic-web/remember"
 import { PrismaClient } from "@prisma/client"
-import chalk from "chalk"
 
 export const prisma = remember("prisma", () => {
   const logThreshold = 50
@@ -16,20 +15,10 @@ export const prisma = remember("prisma", () => {
 
   client.$on("query", async e => {
     if (e.duration < logThreshold) return
-    const color =
-      e.duration < logThreshold * 1.1
-        ? "green"
-        : e.duration < logThreshold * 1.2
-          ? "blue"
-          : e.duration < logThreshold * 1.3
-            ? "yellow"
-            : e.duration < logThreshold * 1.4
-              ? "redBright"
-              : "red"
 
-    const dur = chalk[color](`${e.duration}ms`)
-    console.info(`prisma:query - ${dur} - ${e.query}`)
+    console.info(`prisma:query - ${e.duration}ms - ${e.query}`)
   })
+
   client.$connect()
   return client
 })
