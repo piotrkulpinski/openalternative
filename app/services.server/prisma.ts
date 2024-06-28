e1c2bf3c4c2557d9b637e44a19e7441d601e4f51
Piotr Kulpinski <piotr@kulpinski.pl>
2024-06-28 12:03:11 +0200

    try to fix edge again

--- app/services.server/prisma.ts
+++ app/services.server/prisma.ts
@@ -1,10 +1,33 @@
 import { remember } from "@epic-web/remember"
 import { PrismaClient } from "@prisma/client"
+import { PrismaClient as PrismaClientEdge } from "@prisma/client/edge"
 
 export const prisma = remember("prisma", () => {
   const logThreshold = 50
 
   const client = new PrismaClient({
+    // adapter,
+    log: [
+      { level: "query", emit: "event" },
+      { level: "error", emit: "stdout" },
+      { level: "warn", emit: "stdout" },
+    ],
+  })
+
+  client.$on("query", async e => {
+    if (e.duration < logThreshold) return
+
+    console.info(`prisma:query - ${e.duration}ms - ${e.query}`)
+  })
+
+  client.$connect()
+  return client
+})
+
+export const prismaEdge = remember("prismaEdge", () => {
+  const logThreshold = 50
+
+  const client = new PrismaClientEdge({
     // adapter,
     log: [
       { level: "query", emit: "event" },
