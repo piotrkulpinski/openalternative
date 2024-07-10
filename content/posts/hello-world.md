a8fd997565d03c8985dd7caeef4459f07d310409
Piotr Kulpinski <piotr@kulpinski.pl>
2024-07-10 12:09:40 +0200

    implement content collections

---
+++ content/posts/hello-world.md
@@ -0,0 +1,9 @@
+---
+title: "Hello world"
+description: "This is my first post!"
+---
+
+# Hello world
+
+This is my first post!
+... rest of the content
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { installGlobals } from "@remix-run/node"
 import { vercelPreset } from "@vercel/remix/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
+import contentCollections from "@content-collections/remix-vite"
 
 installGlobals()
 
@@ -10,5 +11,5 @@ export default defineConfig({
   ssr: {
     noExternal: ["tailwind-merge", "remix-utils"],
   },
-  plugins: [tsconfigPaths(), remix({ presets: [vercelPreset()] })],
+  plugins: [tsconfigPaths(), remix({ presets: [vercelPreset()] }), contentCollections()],
 })
