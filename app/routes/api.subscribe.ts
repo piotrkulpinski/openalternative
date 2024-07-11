33688c599d9bf17a489317d52f7248598aec65ac
Piotr Kulpinski <piotr@kulpinski.pl>
2024-07-11 09:58:29 +0200

    add the disposable domain check before subscribing

--- app/routes/api.subscribe.ts
+++ app/routes/api.subscribe.ts
@@ -2,8 +2,20 @@ import { type ActionFunctionArgs, type TypedResponse, json } from "@remix-run/no
 import { got } from "got"
 import { type ZodFormattedError, z } from "zod"
 
+const isRealEmail = async (email: string) => {
+  const disposableJsonURL =
+    "https://rawcdn.githack.com/disposable/disposable-email-domains/master/domains.json"
+  const response = await got(disposableJsonURL).json<string[]>()
+  const domain = email.split("@")[1]
+
+  return !response.includes(domain)
+}
+
 const subscriberSchema = z.object({
-  email: z.string().email("Invalid email address, please use a correct format."),
+  email: z
+    .string()
+    .email("Invalid email address, please use a correct format.")
+    .refine(isRealEmail, "Invalid email address, please use a real one."),
   groups: z.array(z.string()).optional(),
 })
 
@@ -13,7 +25,7 @@ export type ActionState =
 
 export async function action({ request }: ActionFunctionArgs): Promise<TypedResponse<ActionState>> {
   const data = await request.formData()
-  const parsed = subscriberSchema.safeParse(Object.fromEntries(data.entries()))
+  const parsed = await subscriberSchema.safeParseAsync(Object.fromEntries(data.entries()))
 
   if (!parsed.success) {
     return json({ type: "error", error: parsed.error.format() })
