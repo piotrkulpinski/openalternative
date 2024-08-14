a0fb89f277e2593dc3edc89194b73bb97f91650e
Piotr Kulpinski <piotr@kulpinski.pl>
2024-08-14 11:18:45 +0200

    move tool sidebar to separate component

---
+++ app/partials/ToolSidebar.tsx
@@ -0,0 +1,28 @@
+import type { SerializeFrom } from "@remix-run/node"
+import type { HTMLAttributes } from "react"
+import { RepositoryDetails } from "~/components/RepositoryDetails"
+import { SponsoredCard } from "~/components/SponsoredCard"
+import type { LanguageToToolMany, ToolOne } from "~/services.server/api"
+import { HOSTING_SPONSOR } from "~/utils/constants"
+import { cx } from "~/utils/cva"
+
+type ToolSidebarProps = HTMLAttributes<HTMLDivElement> & {
+  tool: SerializeFrom<ToolOne>
+  languages: SerializeFrom<LanguageToToolMany[]>
+}
+
+export const ToolSidebar = ({
+  children,
+  className,
+  tool,
+  languages,
+  ...props
+}: ToolSidebarProps) => {
+  return (
+    <div className={cx("flex flex-col gap-6 md:gap-4", className)} {...props}>
+      <RepositoryDetails tool={tool} languages={languages} />
+      <SponsoredCard sponsoring={HOSTING_SPONSOR} />
+      {children}
+    </div>
+  )
+}
