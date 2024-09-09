5b1bdfeb0b7702cc89b19f7ba182ecd80910ddf9
Piotr Kulpinski <piotr@kulpinski.pl>
2024-09-09 15:03:56 +0200

    add missing packages and update tool schema

--- apps/admin/utils/helpers.ts
+++ apps/admin/utils/helpers.ts
@@ -24,3 +24,16 @@ export function composeEventHandlers<E>(
     }
   }
 }
+
+export function nullsToUndefined<T>(obj: T) {
+  if (obj === null || obj === undefined) {
+    return undefined as any
+  }
+
+  if ((obj as any).constructor.name === "Object" || Array.isArray(obj)) {
+    for (const key in obj) {
+      obj[key] = nullsToUndefined(obj[key]) as any
+    }
+  }
+  return obj as any
+}
