22bb0993e65ad1b68a839e7a472590b8006a0b66
Piotr Kulpinski <piotr@kulpinski.pl>
2024-09-12 22:05:02 +0200

    Versioning untracked files

--- apps/admin/components/ui/Skeleton.tsx
+++
@@ -1,7 +0,0 @@
-import { cx } from "~/utils/cva"
-
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cx("animate-pulse rounded-md bg-foreground/5", className)} {...props} />
-}
-
-export { Skeleton }
