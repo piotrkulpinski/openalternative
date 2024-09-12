ce4153916373f18b2c868764c6dd1d38881f3ca1
Piotr Kulpinski <piotr@kulpinski.pl>
2024-09-12 21:55:06 +0200

    remove Providers.tsx file

--- apps/admin/app/(dashboard)/Providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import type { PropsWithChildren } from "react"
-import { TooltipProvider } from "~/components/ui/tooltip"
-
-export function Providers({ children }: PropsWithChildren) {
-  return <TooltipProvider delayDuration={250}>{children}</TooltipProvider>
-}
