3eab189a6dff0391e5c4477b0ea51c35ae351cc0
Piotr Kulpinski <piotr@kulpinski.pl>
2024-09-23 14:23:47 +0200

    update cache headers

--- apps/web/app/utils/constants.ts
+++ apps/web/app/utils/constants.ts
@@ -68,9 +68,6 @@ export const BANNER_SPONSOR: SponsoringOne = {
   faviconUrl: "https://s3.us-east-1.amazonaws.com/openalternative/polar/favicon.png",
 }
 
-export const JSON_HEADERS =
-  import.meta.env.NODE_ENV === "development"
-    ? {
-        "Cache-Control": "public, max-age=3600, s-maxage=7200 stale-while-revalidate=3.154e7",
-      }
-    : undefined
+export const JSON_HEADERS = {
+  "Cache-Control": "public, max-age=3600, s-maxage=7200 stale-while-revalidate=3.154e7",
+}
