94487665d00d53ddd8866f46fee7cd32088291e0
Piotr Kulpinski <piotr@kulpinski.pl>
2024-09-30 15:44:53 +0200

    simplify the ping component

--- apps/web/app/components/ui/ping.tsx
+++ apps/web/app/components/ui/ping.tsx
@@ -1,20 +1,12 @@
 import type { HTMLAttributes } from "react"
 import { cx } from "~/utils/cva"
 
-type PingProps = HTMLAttributes<HTMLElement> & {
-  label?: string
-}
-
-export const Ping = ({ className, label, ...props }: PingProps) => {
+export const Ping = ({ className, children, ...props }: HTMLAttributes<HTMLElement>) => {
   return (
-    <div className={cx("flex items-center gap-1", className)} {...props}>
-      <div className="relative size-3">
-        <div className="absolute inset-0 animate-ping rounded-full bg-primary/30" />
-        <div className="absolute inset-0 animate-pulse rounded-full bg-primary/30" />
-        <div className="absolute inset-[3px] rounded-full bg-primary" />
-      </div>
-
-      {label && <div className="font-medium uppercase text-primary">{label}</div>}
+    <div className={cx("relative size-3 text-primary", className)} {...props}>
+      <div className="absolute inset-0 animate-ping rounded-full bg-current opacity-30" />
+      <div className="absolute inset-0 animate-pulse rounded-full bg-current opacity-30" />
+      <div className="absolute inset-[3px] rounded-full bg-current" />
     </div>
   )
 }
