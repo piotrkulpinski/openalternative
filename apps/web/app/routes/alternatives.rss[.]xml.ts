be1adfe8216207e1258eab31262853ceee4c1b7d
Piotr Kulpinski <piotr@kulpinski.pl>
2024-09-30 23:18:59 +0200

    clean up alternatives rss feed file

--- apps/web/app/routes/alternatives.rss[.]xml.ts
+++ apps/web/app/routes/alternatives.rss[.]xml.ts
@@ -1,41 +1,37 @@
+import RSS from "rss"
 import { prisma } from "~/services.server/prisma"
-import { SITE_NAME, SITE_TAGLINE } from "~/utils/constants"
+import { SITE_NAME, SITE_TAGLINE, SITE_URL } from "~/utils/constants"
 import { addUTMTracking } from "~/utils/helpers"
 
 export const loader = async () => {
-  const url = import.meta.env.NEXT_PUBLIC_SITE_URL ?? ""
-
   const alternatives = await prisma.alternative.findMany({
     orderBy: { createdAt: "desc" },
     select: { id: true, name: true, slug: true, description: true, createdAt: true },
     take: 50,
   })
 
-  const feed = `<?xml version="1.0" encoding="UTF-8"?>
-  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-    <channel>
-      <title>${SITE_NAME}</title>
-      <description>${SITE_TAGLINE}</description>
-      <link>${addUTMTracking(`${url}/alternatives`, { source: "rss" })}</link>
-      <language>en-us</language>
-      <ttl>60</ttl>
-      <atom:link href="${url}/alternatives/rss.xml" rel="self" type="application/rss+xml" />
-      ${alternatives
-        .map(
-          alternative => `
-      <item>
-        <title><![CDATA[${alternative.name}]]></title>
-        <description><![CDATA[${alternative.description}]]></description>
-        <pubDate>${alternative.createdAt?.toUTCString()}</pubDate>
-        <link>${addUTMTracking(`${url}/alternatives/${alternative.slug}`, { source: "rss" })}</link>
-        <guid isPermaLink="false">${alternative.id}</guid>
-      </item>`,
-        )
-        .join("\n")}
-    </channel>
-  </rss>`
+  const feed = new RSS({
+    title: SITE_NAME,
+    description: SITE_TAGLINE,
+    site_url: addUTMTracking(`${SITE_URL}/alternatives`, { source: "rss" }),
+    feed_url: `${SITE_URL}/alternatives/rss.xml`,
+    copyright: `${new Date().getFullYear()} ${SITE_NAME}`,
+    language: "en",
+    ttl: 14400,
+    pubDate: new Date(),
+  })
 
-  return new Response(feed, {
+  alternatives.map(alternative => {
+    feed.item({
+      guid: alternative.id,
+      title: alternative.name,
+      url: addUTMTracking(`${SITE_URL}/alternatives/${alternative.slug}`, { source: "rss" }),
+      date: alternative.createdAt.toUTCString(),
+      description: alternative.description ?? "",
+    })
+  })
+
+  return new Response(feed.xml({ indent: true }), {
     headers: {
       "Content-Type": "application/xml",
       "X-Content-Type-Options": "nosniff",
