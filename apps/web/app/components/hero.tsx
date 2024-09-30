71560f4fae76ce32176d807186bdaec89a9ed9aa
Piotr Kulpinski <piotr@kulpinski.pl>
2024-09-30 15:44:53 +0200

    simplify the ping component

--- apps/web/app/components/hero.tsx
+++ apps/web/app/components/hero.tsx
@@ -32,9 +32,10 @@ export const Hero = ({ className, toolCount, ...props }: HeroProps) => {
             to="https://go.openalternative.co/peerlist"
             target="_blank"
             rel="noopener noreferrer nofollow"
-            className="-mt-4 mb-2 mx-auto order-first hover:opacity-80 transition-all duration-300"
+            className="relative block -mt-4 mb-2 mx-auto order-first hover:opacity-80 transition-all duration-300"
           >
             <PeerlistIcon className="w-32 h-10" />
+            <Ping className="absolute -top-3 left-1/2 -translate-x-1/2 size-2.5" />
           </Link>
         ) : (
           <Badge
