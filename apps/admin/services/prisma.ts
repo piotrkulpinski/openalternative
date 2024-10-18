44eb921185a0b1fdb25fd3f57ea9df074a2d6fdf
Piotr Kulpinski <piotr@kulpinski.pl>
2024-10-18 20:42:13 +0200

    revert the prisma connection changes

--- apps/admin/services/prisma.ts
+++ apps/admin/services/prisma.ts
@@ -1,16 +1,15 @@
-import { Pool, neonConfig } from "@neondatabase/serverless"
-import { PrismaNeon } from "@prisma/adapter-neon"
 import { PrismaClient } from "@prisma/client"
-import dotenv from "dotenv"
-import ws from "ws"
-import { env } from "~/env"
 
-// Setup
-dotenv.config()
-neonConfig.webSocketConstructor = ws
-const connectionString = `${env.DATABASE_URL}`
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-// Init prisma client
-const pool = new Pool({ connectionString })
-const adapter = new PrismaNeon(pool)
-export const prisma = new PrismaClient({ adapter })
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export { prisma }
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma
--- apps/web/app/services.server/prisma.ts
+++ apps/web/app/services.server/prisma.ts
@@ -1,15 +1,6 @@
-import { Pool, neonConfig } from "@neondatabase/serverless"
-import { PrismaNeon } from "@prisma/adapter-neon"
-import { PrismaClient } from "@prisma/client"
-import dotenv from "dotenv"
-import ws from "ws"
+import { remember } from "@epic-web/remember"
+import { PrismaClient } from "@openalternative/db"
 
-// Setup
-dotenv.config()
-neonConfig.webSocketConstructor = ws
-const connectionString = `${process.env.DATABASE_URL}`
-
-// Init prisma client
-const pool = new Pool({ connectionString })
-const adapter = new PrismaNeon(pool)
-export const prisma = new PrismaClient({ adapter })
+export const prisma = remember("prisma", () => {
+  return new PrismaClient()
+})
