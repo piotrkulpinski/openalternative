43c0ad60cfa9928439a5aa10644dab0f7f2bd727
Piotr Kulpinski <piotr@kulpinski.pl>
2024-10-18 14:05:34 +0200

    update prisma connections

--- apps/web/app/services.server/prisma.ts
+++ apps/web/app/services.server/prisma.ts
@@ -1,6 +1,15 @@
-import { remember } from "@epic-web/remember"
-import { PrismaClient } from "@openalternative/db"
+import { Pool, neonConfig } from "@neondatabase/serverless"
+import { PrismaNeon } from "@prisma/adapter-neon"
+import { PrismaClient } from "@prisma/client"
+import dotenv from "dotenv"
+import ws from "ws"
 
-export const prisma = remember("prisma", () => {
-  return new PrismaClient()
-})
+// Setup
+dotenv.config()
+neonConfig.webSocketConstructor = ws
+const connectionString = `${process.env.DATABASE_URL}`
+
+// Init prisma client
+const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon(pool)
+export const prisma = new PrismaClient({ adapter })
