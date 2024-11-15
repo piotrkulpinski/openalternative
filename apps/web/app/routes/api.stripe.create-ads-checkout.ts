c4940294c0c4940fec9a74aff16575b6f15fdd4f
Piotr Kulpinski <piotr@kulpinski.pl>
2024-11-15 15:52:16 +0100

    add support for multiple ad types in the checkout form

---
+++ apps/web/app/routes/api.stripe.create-ads-checkout.ts
@@ -0,0 +1,82 @@
+import { type ActionFunctionArgs, json } from "@remix-run/node"
+import { z } from "zod"
+import { stripe } from "~/services.server/stripe"
+
+export const stripeAdsCheckoutSchema = z.object({
+  selections: z.array(
+    z.object({
+      type: z.enum(["Homepage", "Banner"]),
+      price: z.coerce.number(),
+      duration: z.coerce.number(),
+      metadata: z.object({
+        startDate: z.coerce.number(),
+        endDate: z.coerce.number(),
+      }),
+    }),
+  ),
+})
+
+export type StripeAdsCheckoutSchema = z.infer<typeof stripeAdsCheckoutSchema>
+
+export async function action({ request }: ActionFunctionArgs) {
+  const data = await request.json()
+  const { selections } = stripeAdsCheckoutSchema.parse(data)
+  const customer = await stripe.customers.create()
+
+  const checkout = await stripe.checkout.sessions.create({
+    mode: "payment",
+    customer: customer.id,
+    customer_update: {
+      name: "auto",
+      address: "auto",
+    },
+    line_items: selections.map(({ type, price, duration }) => ({
+      price_data: {
+        unit_amount: Math.round(price * 100),
+        currency: "usd",
+        // product: process.env.STRIPE_PRODUCT_ID,
+        product_data: { name: `${type} Advertisement` },
+      },
+      quantity: duration,
+    })),
+    custom_fields: [
+      {
+        key: "name",
+        label: { type: "custom", custom: "Company Name" },
+        type: "text",
+      },
+      {
+        key: "description",
+        label: { type: "custom", custom: "Description" },
+        type: "text",
+        optional: true,
+      },
+      {
+        key: "website",
+        label: { type: "custom", custom: "Website URL" },
+        type: "text",
+      },
+    ],
+    metadata: {
+      selections: JSON.stringify(
+        selections.map(({ type, metadata }) => ({
+          type,
+          startDate: metadata.startDate,
+          endDate: metadata.endDate,
+        })),
+      ),
+    },
+    allow_promotion_codes: true,
+    automatic_tax: { enabled: true },
+    tax_id_collection: { enabled: true },
+    invoice_creation: { enabled: true },
+    success_url: `${process.env.NEXT_PUBLIC_SITE_URL}/advertise?subscribed=true`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SITE_URL}/advertise?cancelled=true`,
+  })
+
+  if (!checkout.url) {
+    throw new Error("Unable to create a new Stripe Checkout Session.")
+  }
+
+  return json({ url: checkout.url })
+}
