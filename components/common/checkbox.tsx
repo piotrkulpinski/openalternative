948aeb308567ac5a8b31e93e1c379ff964569b4b
Piotr Kulpinski <piotr@kulpinski.pl>
2024-12-03 12:09:02 +0100

    update checkbox styles

--- components/common/checkbox.tsx
+++ components/common/checkbox.tsx
@@ -1,22 +1,22 @@
 "use client"
 
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { CheckIcon } from "lucide-react"
+import { Check } from "lucide-react"
 import type { ComponentProps } from "react"
 import { Box } from "~/components/common/box"
 import { cx } from "~/utils/cva"
 
 const Checkbox = ({ className, ...props }: ComponentProps<typeof CheckboxPrimitive.Root>) => (
-  <Box hover focusWithin>
+  <Box focusWithin>
     <CheckboxPrimitive.Root
       className={cx(
-        "peer size-4 shrink-0 rounded-sm disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:text-foreground",
+        "peer size-4 shrink-0 rounded-sm disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-foreground data-[state=checked]:text-background",
         className,
       )}
       {...props}
     >
-      <CheckboxPrimitive.Indicator className={cx("flex items-center justify-center text-current")}>
-        <CheckIcon className="size-3" />
+      <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current">
+        <Check className="size-4" />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   </Box>
