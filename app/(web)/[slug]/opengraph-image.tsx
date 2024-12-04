f0240855b4a7964978ac9e5f36b14da33d4d846d
Piotr Kulpinski <piotr@kulpinski.pl>
2024-12-04 14:15:21 +0100

    fix issues with og images

--- app/(web)/[slug]/opengraph-image.tsx
+++ app/(web)/[slug]/opengraph-image.tsx
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation"
 import { ImageResponse } from "next/og"
 import { OgBase } from "~/components/web/og/og-base"
 import { loadGoogleFont } from "~/lib/fonts"
-import { findToolBySlug } from "~/server/web/tools/queries"
+import { prisma } from "~/services/prisma"
 
 type PageProps = {
   params: Promise<{ slug: string }>
@@ -15,7 +15,7 @@ export const size = { width: 1200, height: 630 }
 
 export default async function Image({ params }: PageProps) {
   const { slug } = await params
-  const tool = await findToolBySlug(slug, {})
+  const tool = await prisma.tool.findFirst({ where: { slug } })
 
   if (!tool) {
     notFound()
