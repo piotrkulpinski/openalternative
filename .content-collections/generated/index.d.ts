97810a4072af24498b4726180e88fec4ebc574f4
Piotr Kulpinski <piotr@kulpinski.pl>
2024-12-16 15:29:46 +0100

    removed old .next cache from the repository

--- .content-collections/generated/index.d.ts
+++
@@ -1,7 +0,0 @@
-import configuration from "../../apps/web/content-collections.js";
-import { GetTypeByName } from "@content-collections/core";
-
-export type Post = GetTypeByName<typeof configuration, "posts">;
-export declare const allPosts: Array<Post>;
-
-export {};
