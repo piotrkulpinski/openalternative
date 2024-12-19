bb75c7a1a401b718e046205722b3342d4cc6767e
Piotr Kulpinski <piotr@kulpinski.pl>
2024-12-19 10:18:35 +0100

    secure the admin next actions

--- apps/web/src/actions/misc.ts
+++ apps/web/src/actions/misc.ts
@@ -1,13 +1,18 @@
 "use server"
 
 import { prisma } from "@openalternative/db"
+import { z } from "zod"
+import { authedProcedure } from "~/lib/safe-actions"
 import { getPostTemplate, sendSocialPost } from "~/lib/socials"
 
-export const testSocialPosts = async () => {
-  const tool = await prisma.tool.findFirst({ where: { slug: "dub" } })
+export const testSocialPosts = authedProcedure
+  .createServerAction()
+  .input(z.object({ slug: z.string() }))
+  .handler(async ({ input: { slug } }) => {
+    const tool = await prisma.tool.findFirst({ where: { slug } })
 
-  if (tool) {
-    const template = await getPostTemplate(tool)
-    return sendSocialPost(template, tool)
-  }
-}
+    if (tool) {
+      const template = await getPostTemplate(tool)
+      return sendSocialPost(template, tool)
+    }
+  })
