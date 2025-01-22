25e3e8e7d8660660237626602011363d803ac489
Piotr Kulpinski <piotr@kulpinski.pl>
2025-01-22 17:35:52 +0100

    add category filter on self-hosted page

--- apps/web/src/app/(web)/self-hosted/listing.tsx
+++ apps/web/src/app/(web)/self-hosted/listing.tsx
@@ -1,5 +1,6 @@
 import type { SearchParams } from "nuqs/server"
 import { ToolQuery } from "~/components/web/tools/tool-query"
+import { findCategories } from "~/server/web/categories/queries"
 import { searchTools } from "~/server/web/tools/queries"
 import { toolsSearchParamsCache } from "~/server/web/tools/search-params"
 
@@ -10,15 +11,17 @@ type SelfHostedToolListingProps = {
 export const SelfHostedToolListing = async ({ searchParams }: SelfHostedToolListingProps) => {
   const parsedParams = toolsSearchParamsCache.parse(await searchParams)
 
-  const { tools, totalCount } = await searchTools(parsedParams, {
-    where: { isSelfHosted: true },
-  })
+  const [{ tools, totalCount }, categories] = await Promise.all([
+    searchTools(parsedParams, { where: { isSelfHosted: true } }),
+    findCategories({}),
+  ])
 
   return (
     <ToolQuery
       tools={tools}
       totalCount={totalCount}
       perPage={parsedParams.perPage}
+      categories={categories}
       placeholder={`Search in ${totalCount} self-hosted tools...`}
     />
   )
