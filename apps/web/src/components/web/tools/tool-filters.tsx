d7aad9c0a601bf65babe0b5fef27d211f5410be6
Piotr Kulpinski <piotr@kulpinski.pl>
2025-01-25 00:05:59 +0100

    add tool refinements

--- apps/web/src/components/web/tools/tool-filters.tsx
+++ apps/web/src/components/web/tools/tool-filters.tsx
@@ -1,16 +1,38 @@
 "use client"
 
-import type { HTMLAttributes } from "react"
+import type { Values } from "nuqs"
+import { type HTMLAttributes, useEffect } from "react"
+import { useServerAction } from "zsa-react"
+import { ToolRefinement } from "~/components/web/tools/tool-refinement"
+import { config } from "~/config"
+import { findFilterOptions } from "~/server/web/tools/actions"
+import type { toolsSearchParams } from "~/server/web/tools/search-params"
 import { cx } from "~/utils/cva"
 
-export const ToolFilters = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
+export type ToolFiltersProps = HTMLAttributes<HTMLDivElement> & {
+  filters: Values<typeof toolsSearchParams>
+  updateFilters: (values: Partial<Values<typeof toolsSearchParams>>) => void
+}
+
+export const ToolFilters = ({ className, filters, updateFilters, ...props }: ToolFiltersProps) => {
+  const { execute, isPending, data } = useServerAction(findFilterOptions)
+
+  useEffect(() => {
+    execute()
+  }, [execute])
+
   return (
-    <div
-      className={cx(
-        "grid grid-auto-fill-xs justify-between gap-6 w-full py-4 px-6 border rounded-md",
-        className,
-      )}
-      {...props}
-    />
+    <div className={cx("grid grid-cols-xs justify-between gap-4 w-full", className)} {...props}>
+      {config.search.filters.map(filter => (
+        <ToolRefinement
+          key={filter}
+          filter={filter}
+          items={data?.[filter] ?? []}
+          isPending={isPending}
+          filters={filters}
+          updateFilters={updateFilters}
+        />
+      ))}
+    </div>
   )
 }
--- apps/web/src/config/index.ts
+++ apps/web/src/config/index.ts
@@ -2,6 +2,7 @@ import { adsConfig } from "~/config/ads"
 import { dataTableConfig } from "~/config/data-table"
 import { linksConfig } from "~/config/links"
 import { metadataConfig } from "~/config/metadata"
+import { searchConfig } from "~/config/search"
 import { siteConfig } from "~/config/site"
 import { statsConfig } from "~/config/stats"
 import { submissionsConfig } from "~/config/submissions"
@@ -13,5 +14,6 @@ export const config = {
   metadata: metadataConfig,
   ads: adsConfig,
   submissions: submissionsConfig,
+  search: searchConfig,
   dataTable: dataTableConfig,
 }
