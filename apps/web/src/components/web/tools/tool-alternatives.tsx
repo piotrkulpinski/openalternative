1c7f86fd42acf5c4b3a529f76e2584020007f1c4
Piotr Kulpinski <piotr@kulpinski.pl>
2025-02-11 22:53:54 +0100

    clean up hover card component and make sure it's only triggered for alternatives

--- apps/web/src/components/web/tools/tool-alternatives.tsx
+++ apps/web/src/components/web/tools/tool-alternatives.tsx
@@ -1,5 +1,6 @@
 import { joinAsSentence } from "@curiousleaf/utils"
 import { Stack } from "~/components/common/stack"
+import { AlternativeHoverCard } from "~/components/web/alternatives/alternative-hover-card"
 import { BrandLink } from "~/components/web/ui/brand-link"
 import type { AlternativeMany } from "~/server/web/alternatives/payloads"
 
@@ -19,13 +20,14 @@ export const ToolAlternatives = ({ alternatives }: ToolAlternativesProps) => {
       <Stack>
         <span className="text-sm">Open Source Alternative to:</span>
 
-        {alternatives.map(({ slug, name, faviconUrl }) => (
-          <BrandLink
-            key={slug}
-            href={`/alternatives/${slug}`}
-            name={name}
-            faviconUrl={faviconUrl}
-          />
+        {alternatives.map(alternative => (
+          <AlternativeHoverCard key={alternative.slug} alternative={alternative}>
+            <BrandLink
+              href={`/alternatives/${alternative.slug}`}
+              name={alternative.name}
+              faviconUrl={alternative.faviconUrl}
+            />
+          </AlternativeHoverCard>
         ))}
       </Stack>
     </>
