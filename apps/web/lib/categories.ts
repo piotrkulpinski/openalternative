ecd431a80e8c081f44666e24febd2fc23ce788ee
Piotr Kulpinski <piotr@kulpinski.pl>
2025-04-08 14:35:57 +0200

    update category tree queries

--- apps/web/lib/categories.ts
+++ apps/web/lib/categories.ts
@@ -1,20 +1,12 @@
-import { db } from "@openalternative/db"
 import type { Category } from "@openalternative/db/client"
+import { getCategoryAncestors } from "~/server/web/categories/queries"
 
 export const getCategoryPath = async (category: Pick<Category, "slug" | "parentId">) => {
-  const path: string[] = [category.slug]
-  let current = category
-
-  while (current.parentId) {
-    const parent = await db.category.findUnique({
-      where: { id: current.parentId },
-    })
-
-    if (!parent) break
-
-    path.unshift(parent.slug)
-    current = parent
+  if (!category.parentId) {
+    return category.slug
   }
 
-  return path.join("/")
+  const categoryAncestors = await getCategoryAncestors(category.slug)
+
+  return categoryAncestors.join("/")
 }
