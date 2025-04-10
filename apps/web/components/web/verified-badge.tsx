4989fdd84b549e94a0375b09853692e8fefc121c
Piotr Kulpinski <piotr@kulpinski.pl>
2025-04-10 14:17:58 +0200

    refactor: replace Icon component with Image for VerifiedBadge

--- apps/web/components/web/verified-badge.tsx
+++ apps/web/components/web/verified-badge.tsx
@@ -1,19 +1,21 @@
+import Image from "next/image"
 import type { ComponentProps } from "react"
-import { Icon } from "~/components/common/icon"
+import VerifiedBadgeIcon from "~/assets/verified-badge.svg"
 import { Tooltip } from "~/components/common/tooltip"
 import { cx } from "~/utils/cva"
 
-type VerifiedBadgeProps = Omit<ComponentProps<typeof Icon>, "name"> & {
+type VerifiedBadgeProps = Omit<ComponentProps<typeof Image>, "src" | "alt"> & {
   size?: "sm" | "md" | "lg"
 }
 
 export const VerifiedBadge = ({ className, size = "md", ...props }: VerifiedBadgeProps) => {
   return (
     <Tooltip tooltip="Verified">
-      <Icon
-        name="lucide/badge-check"
+      <Image
+        src={VerifiedBadgeIcon}
+        alt="Verified"
         className={cx(
-          "-ml-1 fill-blue-500 stroke-background",
+          "-ml-1",
           size === "sm" && "-mb-[0.15em] size-4",
           size === "md" && "-mb-[0.2em] size-5",
           size === "lg" && "-mb-[0.25em] size-6",
