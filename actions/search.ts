8505d20b35c5915e11c93a4f781a8e6d86893bdf
Piotr Kulpinski <piotr@kulpinski.pl>
2025-05-12 10:51:20 +0200

    refactor: enhance search functionality to include additional fields and trim input query

--- actions/search.ts
+++ actions/search.ts
@@ -1,17 +1,26 @@
 "use server"
 
+import { ToolStatus } from "@prisma/client"
 import { z } from "zod"
 import { createServerAction } from "zsa"
 import { db } from "~/services/db"
 
 export const searchItems = createServerAction()
-  .input(z.object({ query: z.string() }))
+  .input(z.object({ query: z.string().trim() }))
   .handler(async ({ input: { query } }) => {
     const start = performance.now()
 
     const [tools, alternatives, categories, licenses] = await Promise.all([
       db.tool.findMany({
-        where: { name: { contains: query, mode: "insensitive" } },
+        where: {
+          status: ToolStatus.Published,
+          OR: [
+            { name: { contains: query, mode: "insensitive" } },
+            { tagline: { contains: query, mode: "insensitive" } },
+            { description: { contains: query, mode: "insensitive" } },
+            { alternatives: { some: { name: { contains: query, mode: "insensitive" } } } },
+          ],
+        },
         orderBy: { name: "asc" },
         take: 10,
       }),
