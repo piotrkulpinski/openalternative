29067ac9c6bbc574125af45ad95b7db8000d05af
Piotr Kulpinski <piotr@kulpinski.pl>
2025-05-19 15:16:31 +0200

    refactor: simplify banner ad component

--- components/web/ads/ad-banner.tsx
+++ components/web/ads/ad-banner.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image"
 import type { ComponentProps } from "react"
 import { Badge } from "~/components/common/badge"
-import { Box } from "~/components/common/box"
 import { Button } from "~/components/common/button"
+import { Card } from "~/components/common/card"
 import { ExternalLink } from "~/components/web/external-link"
 import { Container } from "~/components/web/ui/container"
 import { findAd } from "~/server/web/ads/queries"
 import { cx } from "~/utils/cva"
 
-export const AdBanner = async ({ className, ...props }: ComponentProps<typeof Container>) => {
+export const AdBanner = async ({ className, ...props }: ComponentProps<typeof Card>) => {
   const ad = await findAd({ where: { type: "Banner" } })
 
   if (!ad) {
@@ -16,11 +16,10 @@ export const AdBanner = async ({ className, ...props }: ComponentProps<typeof Co
   }
 
   return (
-    <Box hover focus>
-      <Container
+    <Container asChild>
+      <Card
         className={cx(
-          "group/button relative -top-px z-50 flex items-center justify-between gap-3 pt-2 pb-2.5 bg-card hover:bg-accent lg:rounded-b-lg",
-          "before:absolute before:inset-0 before:-z-1 before:rounded-[inherit] before:border-4 before:border-t-0 before:border-background",
+          "-top-px z-50 flex-row items-center gap-3 pt-2 pb-2.5 rounded-t-none max-lg:rounded-b-none before:border-t-0",
           className,
         )}
         asChild
@@ -57,7 +56,7 @@ export const AdBanner = async ({ className, ...props }: ComponentProps<typeof Co
             <span>{ad.buttonLabel ?? "Learn More"}</span>
           </Button>
         </ExternalLink>
-      </Container>
-    </Box>
+      </Card>
+    </Container>
   )
 }
