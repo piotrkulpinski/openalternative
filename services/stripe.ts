b523f7645d3036ea9118dcfdf4c1ee173e5d6729
Piotr Kulpinski <piotr@kulpinski.pl>
2025-06-02 15:58:13 +0200

    chore: update packages

--- services/stripe.ts
+++ services/stripe.ts
@@ -2,5 +2,5 @@ import Stripe from "stripe"
 import { env } from "~/env"
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-04-30.basil",
+  apiVersion: "2025-05-28.basil",
 })
